567747c8b0ea0f5b54c439093cde8d2a4a7dc696
hmy751 <hmy751@gmail.com>
2023-09-03 21:02:56 +0900

    feature: storybook 예시 Task 컴포넌트 및 스토리 추가

---
+++ src/stories/Task/Task.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
+  return (
+    <div className="list-item">
+      <input type="text" value={title} readOnly={true} />
+    </div>
+  );
+}
