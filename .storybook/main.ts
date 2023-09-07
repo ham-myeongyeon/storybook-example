4a9498ba8c1b814e2702e70c35dbe3805f328c09
hmy751 <hmy751@gmail.com>
2023-09-08 07:36:09 +0900

    chore: babel 및 typescript 관련 main.ts 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,8 @@
-module.exports = {
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   // typescript: {
@@ -11,23 +15,38 @@ module.exports = {
   //     propFilter: () => true,
   //   },
   // },
+  // typescript: {
+  //   check: true,
+  //   skipBabel: true,
+  //   reactDocgen: "react-docgen-typescript",
+  // },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
+    // "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
   ],
-
-  features: {
-    postcss: false,
-  },
-
+  // features: {
+  //   postcss: false,
+  // },
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-
   docs: {
     autodocs: true,
   },
+  // webpackFinal: async (config) => {
+  //   if (config.resolve) {
+  //     config.resolve.plugins = [
+  //       ...(config.resolve.plugins || []),
+  //       new TsconfigPathsPlugin({
+  //         extensions: config.resolve.extensions,
+  //       }),
+  //     ];
+  //   }
+  //   return config;
+  // },
 };
+
+export default config;
