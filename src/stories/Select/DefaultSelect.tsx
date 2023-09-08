77a461009346f89018e8b073a4c82976aa75966a
hmy751 <hmy751@gmail.com>
2023-09-08 09:03:51 +0900

    feature: Select 컴포넌트 생성

---
+++ src/stories/Select/DefaultSelect.tsx
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import ArrowDownSmallIcon from "src/components/Icon/ArrowDownSmallIcon";
+import { SelectContext } from "src/stories/Select/Select";
+
+const SelectBox = styled.div<{
+  width?: string;
+  selectValidation: boolean;
+  disabled: boolean;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: ${({ width }) => width || "100%"};
+  height: 40px;
+  border-radius: 5px;
+  padding: 0 10px;
+  opacity: ${({ disabled }) => (disabled ? "0.6" : "")};
+  border: ${({ selectValidation }) => `
+    1px solid ${selectValidation ? "#d8d8d8" : "#ff4c4c"}
+  `};
+  cursor: ${({ disabled }) => (disabled ? "default" : "")};
+  background-color: ${({ disabled }) => (disabled ? "#f7f7f7" : "")};
+`;
+
+interface DefaultSelectProps {
+  width?: string;
+  selectValidation?: boolean;
+  renderSelectContent: () => string | number;
+}
+
+const DefaultSelect = ({
+  width,
+  selectValidation = true,
+  renderSelectContent,
+}: DefaultSelectProps) => {
+  const { disabled } = useContext(SelectContext);
+
+  return (
+    <SelectBox
+      selectValidation={selectValidation}
+      disabled={disabled}
+      width={width}
+    >
+      {renderSelectContent && typeof renderSelectContent === "function"
+        ? renderSelectContent()
+        : ""}
+      <ArrowDownSmallIcon width="12px" height="10px" />
+    </SelectBox>
+  );
+};
+
+export default DefaultSelect;
