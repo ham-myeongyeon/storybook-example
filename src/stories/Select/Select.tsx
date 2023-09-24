4dc68b3f8ff613e02cd669055f11e777041a868d
hmy751 <hmy751@gmail.com>
2023-09-24 09:57:25 +0900

    feature: Select 컴포넌트 및 Trigger, Value, Option 생성

--- src/stories/Select/Select.tsx
+++ src/stories/Select/Select.tsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { createContext } from "react";
 
-interface SelectProps {}
+
+/*
+* Select
+*/
+
+interface SelectContext {};
+
+const SelectContext = React.createContext<SelectContext>({});
+
+interface SelectProps extends React.PropsWithChildren {}
 
 const Select: React.FC<SelectProps> = (props) => {
-  const {} = props;
-  return <></>;
+  const { children } = props;
+  const SelectContextValue: SelectContext = React.useMemo({
+    
+  }. []);
+
+
+  return (
+    <>
+      <SelectContext.Provider value={SelectContextValue}>{children}</SelectContext.Provider>
+    </>
+  );
 };
+
+/*
+*  Trigger
+*/
+
+interface TriggerProps {};
+
+const Trigger: React.FC<TriggerProps> = (props) => {
+  const {} = props;
+  const {} = React.useContext(SelectContext);
+
+  return <div></div>
+}
+
+/*
+*  Value
+*/
+
+interface ValueProps {};
+
+const Value: React.FC<ValueProps> = (props) => {
+  const {} = props;
+  const {} = React.useContext(SelectContext);
+
+  return <div></div>
+}
+
+/*
+*  Option
+*/
+
+interface OptionProps {};
+
+const Option: React.FC<OptionProps> = (props) => {
+  const {} = props;
+  const {} = React.useContext(SelectContext);
+
+  return <div></div>
+}
+
+
+export {
+  Select,
+  Trigger,
+  Value,
+  Option
+}
 
 export default Select;
