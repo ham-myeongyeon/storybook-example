45d733c5c6e6bef645a64113bd58d934539007bb
hmy751 <hmy751@gmail.com>
2023-09-30 00:17:47 +0900

    feature: trigger, option 기본형태 구현

--- src/stories/Select/Select.tsx
+++ src/stories/Select/Select.tsx
@@ -1,75 +1,111 @@
-import React, { createContext } from "react";
-
+import React from "react";
 
 /*
-* Select
-*/
+ * Select
+ */
 
-interface SelectContext {};
+type OptionMap = Map<string | number, string | React.ReactNode>;
+interface SelectContext {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  optionMapRef: React.MutableRefObject<OptionMap | null>;
+}
 
-const SelectContext = React.createContext<SelectContext>({});
+const SelectContext = React.createContext<SelectContext>({} as SelectContext);
 
 interface SelectProps extends React.PropsWithChildren {}
 
 const Select: React.FC<SelectProps> = (props) => {
   const { children } = props;
-  const SelectContextValue: SelectContext = React.useMemo({
-    
-  }. []);
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const optionMapRef = React.useRef<OptionMap | null>(null);
 
+  const SelectContextValue: SelectContext = React.useMemo(
+    () => ({
+      isOpen,
+      setIsOpen,
+      optionMapRef,
+    }),
+    [isOpen, setIsOpen]
+  );
 
   return (
     <>
-      <SelectContext.Provider value={SelectContextValue}>{children}</SelectContext.Provider>
+      <SelectContext.Provider value={SelectContextValue}>
+        {children}
+      </SelectContext.Provider>
     </>
   );
 };
 
 /*
-*  Trigger
-*/
+ * Trigger
+ */
 
-interface TriggerProps {};
+interface TriggerProps {}
 
 const Trigger: React.FC<TriggerProps> = (props) => {
   const {} = props;
-  const {} = React.useContext(SelectContext);
+  const { isOpen, setIsOpen } = React.useContext(SelectContext);
 
-  return <div></div>
-}
+  const handleClickSelect = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return <button onClick={handleClickSelect}></button>;
+};
 
 /*
-*  Value
-*/
+ * Value
+ */
 
-interface ValueProps {};
+interface ValueProps {}
 
 const Value: React.FC<ValueProps> = (props) => {
   const {} = props;
   const {} = React.useContext(SelectContext);
 
-  return <div></div>
-}
+  return <div></div>;
+};
 
 /*
-*  Option
-*/
+ * Content
+ */
 
-interface OptionProps {};
+interface ContentProps {}
+
+const Content: React.FC<ContentProps> = (props) => {
+  const {} = props;
+
+  return <div></div>;
+};
+
+/*
+ * Option
+ */
+
+interface OptionProps extends React.PropsWithChildren {
+  value: string | number;
+  children: React.ReactNode | string;
+}
 
 const Option: React.FC<OptionProps> = (props) => {
-  const {} = props;
-  const {} = React.useContext(SelectContext);
+  const { value, children } = props;
+  const { optionMapRef } = React.useContext(SelectContext);
 
-  return <div></div>
-}
+  React.useEffect(() => {
+    if (!optionMapRef?.current) {
+      optionMapRef.current = new Map();
+    }
 
+    optionMapRef.current.set(value ?? children, children);
+  }, []);
 
-export {
-  Select,
-  Trigger,
-  Value,
-  Option
-}
+  return <option value={value}>{children}</option>;
+};
+
+const Root = Select;
+
+export { Root, Trigger, Value, Content, Option };
 
 export default Select;
